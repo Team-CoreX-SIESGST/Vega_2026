a9687225356f315537ffbb991e813fcfc824f658
UmairMomin <umairmomin04@gmail.com>
2026-02-20 11:57:14 +0530

    fixed api_client

--- client/src/utils/api_client.js
+++ client/src/utils/api_client.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8001/api";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8001/api";
 
 class ApiClient {
   constructor() {
